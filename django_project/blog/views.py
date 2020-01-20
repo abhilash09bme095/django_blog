0fd5422c711b5eef1b99cd021785bffd10314b13
kaabil <abhilashkollikonda@gmail.com>
2020-01-19 22:51:04 -0500

    Tempplates added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author': 'coryMS',
+        'title': 'Blogpost 1',
+        'content': 'first post content',
+        'date_posted': 'Aug 27 2019',
+    },
+    {
+        'author': 'Abhi',
+        'title': 'Blogpost 2',
+        'content': 'Second post content',
+        'date_posted': 'Aug 31 2019',
+    }
+
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> blog about </h1>')
-    
+    return render(request, 'blog/about.html',{'title':'About'})
+
+
+
+
 
 
 
